6bae84cad2e0750fe9a0bc609eb408302793bd85
sjhakjha28 <sjhakjha28@gmail.com>
2021-04-24 22:08:15 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#Recriando a interface da Netflix :red_circle:
+
+Projeto realizado no Bootcamp da Digital Innovation One, recriando a interface da Netflix com um carrosel e responsiva. Utilizando:
+
+:heavy_check_mark: HTML
+
+:heavy_check_mark: CSS
+
+:heavy_check_mark: Javascript
+
+:heavy_check_mark: Plugin Jquery
+
+###Implementado também:
+
+:ballot_box_with_check: Menu lateral para telas pequenas
+
+:ballot_box_with_check: Header fixo durante scroll
---
+++ js/script.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector("header");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
